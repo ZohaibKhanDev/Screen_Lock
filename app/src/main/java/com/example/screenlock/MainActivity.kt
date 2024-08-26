5592509e62814896d4f55e93c43a93c137c4317a
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-08-26 15:27:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/screenlock/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.screenlock
+
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    private lateinit var devicePolicyManager: DevicePolicyManager
+    private lateinit var compName: ComponentName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        devicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        compName = ComponentName(this, MyDeviceAdminReceiver::class.java)
+
+        if (devicePolicyManager.isAdminActive(compName)) {
+            devicePolicyManager.lockNow()
+            finish()
+        } else {
+            val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName)
+            intent.putExtra(
+                DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                "This app requires device admin rights to lock the screen."
+            )
+            startActivityForResult(intent, 1)
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/screenlock/My Device Receiver.kt
@@ -0,0 +1,16 @@
+package com.example.screenlock
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyDeviceAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+    }
+}
